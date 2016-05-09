a0d12b649722aa5b14f550209efe74493e03b349
Scott Peters <speters1@nmsu.edu>
2016-05-09 14:38:46 -0600

    updated Jenns

--- Jenn.java
+++ Jenn.java
@@ -1,5 +1,55 @@
 package FinalProject;
 
-public class Jenn {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.URL;
+import java.net.UnknownHostException;
 
+
+public class Jenn {
+	
+	public static void MyIp() {
+		URL url = null;
+	        BufferedReader in = null;
+	        String ipAddress = "";
+	        try {
+	            url = new URL("http://bot.whatismyipaddress.com");
+	            in = new BufferedReader(new InputStreamReader(url.openStream()));
+	            ipAddress = in.readLine().trim();
+	            if (!(ipAddress.length() > 0)) {
+	                try {
+	                    InetAddress ip = InetAddress.getLocalHost();
+	                    System.out.println((ip.getHostAddress()).trim());
+	                    ipAddress = (ip.getHostAddress()).trim();
+	                } catch(Exception exp) {
+	                    ipAddress = "ERROR";
+	                }
+	            }
+	        } catch (Exception ex) {
+	            // This try will give the Private IP of the Host.
+	            try {
+	                InetAddress ip = InetAddress.getLocalHost();
+	                System.out.println((ip.getHostAddress()).trim());
+	                ipAddress = (ip.getHostAddress()).trim();
+	            } catch(Exception exp) {
+	                ipAddress = "ERROR";
+	            }
+	            //ex.printStackTrace();
+	        }
+	        System.out.println("IP Address: " + ipAddress);
+	    }
+
+public static void test(){
+
+
+	try {
+	  InetAddress addr = InetAddress.getLocalHost();            
+	  System.out.println("Internal Address: " + addr.getHostAddress());
+	} catch (UnknownHostException e) {
+	
+}	
 }
+}
+
+	
